e343d4bbb0e1099f0e91bf1d324ce67067f2ee28
Makers Academy <onboarding@makersacademy.com>
2018-10-18 11:01:04 +0000

    input names

--- app.rb
+++ app.rb
@@ -1,25 +1,13 @@
 require 'sinatra'
 set :session_secret, 'super secret'
 get '/' do
-  'World'
+     erb :form
 end
 
-get '/secret' do
-  'This is a secret page'
-end
-
- get '/random-cat' do
-  @name = "Oscar"
-  erb(:index)
-end
-
-post '/named-cat' do
+post '/name' do
   p params
   @name = params[:name]
   erb :index
 
 end
 
-  get '/cat-form' do
-    erb :cat_form
-end
